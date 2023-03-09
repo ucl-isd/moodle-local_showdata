dbda17828a73fd8ef184e204c08ba107086e7c51
David Kwaw <cybernotic@gmail.com>
2023-03-09 00:25:15 +0000

    update version to work with 311

--- version.php
+++ version.php
@@ -11,7 +11,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2023030801;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2022112801;        // Requires this Moodle version
+$plugin->requires  = 20230224;        // Requires this Moodle version
 $plugin->release   = '0.1';
 $plugin->component = 'local_showdata';  // Full name of the plugin (used for diagnostics)
 $plugin->maturity  = MATURITY_BETA;
